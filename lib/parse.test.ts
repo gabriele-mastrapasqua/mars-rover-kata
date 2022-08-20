0ec740936899cb51f3732035d9a693304ecae9a7
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2022-08-20 15:52:18 +0200

    improve the 1 parse test

--- lib/parse.test.ts
+++ lib/parse.test.ts
@@ -17,11 +17,17 @@ describe("parse test input", () => {
   it("should find a grid of 5 width 4 height", () => {
     const game: GameState = parse(testInputString);
 
-    console.log('got a game ', game.grid)
+    console.log("got a game ", game.grid);
     expect(game).not.toBe(null);
     expect(game.grid.width).toBe(5);
     expect(game.grid.height).toBe(4);
     expect(game.obstacles.length).toBe(3);
     expect(game.commands.length).toBe(3);
+    expect(game.grid.cells.length).toBe(4);
+    expect(game.grid.cells[0].length).toBe(5);
+    expect(game.grid.cells[0]).toEqual(["0", "0", "X", "0", "0"]);
+    expect(game.grid.cells[1]).toEqual(["0", "0", "0", "0", "0"]);
+    expect(game.grid.cells[2]).toEqual(["0", "0", "0", "X", "0"]);
+    expect(game.grid.cells[3]).toEqual(["X", "0", "0", "0", "0"]);
   });
 });
