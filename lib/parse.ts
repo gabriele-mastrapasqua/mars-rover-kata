b7064a07f3d29b26ea8c3f0e80e968ae391b4433
Gabriele Mastrapasqua <gabriele.mastrapasqua@gmail.com>
2022-09-29 16:30:26 +0200

    fix - refactor parse sizes

--- lib/parse.ts
+++ lib/parse.ts
@@ -13,31 +13,36 @@ import {
   InvalidObstaclesError,
 } from './errors'
 import {initialState, newGameState} from './game'
+import {sanitize} from './utils'
 
 type ParseSection = 'none' | 'grid' | 'obstacles' | 'commands'
 
+const parseSizes = (row: string) => {
+  const sizes = row.split(' ')
+  if (sizes.length !== 3) {
+    throw new InvalidGridError(`Bad grid size format: ${row}`)
+  }
+  const gridWidth = parseInt(sizes[1], 10)
+  const gridHeight = parseInt(sizes[2], 10)
+  return [gridWidth, gridHeight]
+}
+
 export const parse = (input: string): GameState => {
-  input = input.replace(/\t/g, '').replace(/  /g, '')
+  input = sanitize(input)
 
   let currentSection: ParseSection = 'none'
 
   const rows = input.split('\n')
-  let [gridWidth, gridHeight] = [0, 0]
+  let sizes = [0, 0]
   let obstacles: Obstacle[] = []
   let commands: CommandSequence[] = []
 
-  rows.forEach((row: string) => {
-    if (row !== '') {
-      //console.log(`section ${currentSection} row '${row}'`);
-
+  rows
+    .filter(row => row !== '')
+    .forEach((row: string) => {
       if (currentSection === 'none' && row.startsWith('Size')) {
         currentSection = 'grid'
-        const sizes = row.split(' ')
-        if (sizes.length !== 3) {
-          throw new InvalidGridError(`Bad grid size format: ${row}`)
-        }
-        gridWidth = parseInt(sizes[1], 10)
-        gridHeight = parseInt(sizes[2], 10)
+        sizes = parseSizes(row)        
       } else if (row.startsWith('Obstacle')) {
         currentSection = 'obstacles'
         const positions = row.split(' ')
@@ -62,8 +67,9 @@ export const parse = (input: string): GameState => {
         validateCommands(commandSequence)
         commands.push(commandSequence as CommandSequence)
       }
-    }
-  })
+    })
+
+  let [gridWidth, gridHeight] = sizes
 
   // basic check for input errors
   if (gridWidth === 0 || gridHeight === 0) {
--- lib/utils.ts
+++ lib/utils.ts
@@ -3,3 +3,8 @@ export const clone = (state: any) => JSON.parse(JSON.stringify(state))
 export const mod = (n: number, m: number) => {
   return ((n % m) + m) % m
 }
+
+
+export const sanitize = (input: string) : string => {
+  return input.replace(/\t/g, '').replace(/  /g, '')
+}
